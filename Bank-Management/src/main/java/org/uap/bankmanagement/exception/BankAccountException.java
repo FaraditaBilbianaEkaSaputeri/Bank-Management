b49b91e5fd911dc28dd466a8035f13027762a28d
FaraditaBilbianaEkaSaputeri <145762944+FaraditaBilbianaEkaSaputeri@users.noreply.github.com>
2024-12-24 13:30:21 +0700

    Add model & exception

---
+++ Bank-Management/src/main/java/org/uap/bankmanagement/exception/BankAccountException.java
@@ -0,0 +1,12 @@
+package org.uap.bankmanagement.exception;
+public class BankAccountException extends Exception
+	{
+		public BankAccountException()
+		{
+			super();
+		}
+		public BankAccountException(String message)
+		{
+			super(message);
+		}
+}
